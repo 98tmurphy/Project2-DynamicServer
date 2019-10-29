385f17b09c4f9193f3f6a84533939c35fcc01613
tauf9157 <55361486+tauf9157@users.noreply.github.com>
2019-10-29 16:32:56 -0500

    Update server.js

--- Project2-DynamicServer/server.js
+++ Project2-DynamicServer/server.js
@@ -27,12 +27,65 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
 app.use(express.static(public_dir));
 
 
+
 // GET request handler for '/'
 app.get('/', (req, res) => {
     ReadFile(path.join(template_dir, 'index.html')).then((template) => {
         let response = template;
         // modify `response` here
-        WriteHtml(res, response);
+        var total_coal = 0;
+        var total_natural_gas = 0;
+        var total_nuclear_count = 0;
+        var total_petroleum_count = 0;
+        var total_renewable_count = 0;
+
+        db.all("SELECT * FROM Consumption WHERE year = ?", ["2017"], (err, rows) => {
+            
+            for (var i = 0; i < rows.length; i++) 
+            {
+                total_coal = total_coal + rows[i].coal;
+                total_natural_gas = total_natural_gas + rows[i].natural_gas;
+                total_nuclear_count = total_nuclear_count + rows[i].nuclear;
+                total_petroleum_count = total_petroleum_count + rows[i].petroleum;
+                total_renewable_count = total_renewable_count + rows[i].renewable;
+            }
+
+            response = response.replace('var coal_count', 'var coal_count = ' + total_coal.toString());
+            response = response.replace('var natural_gas_count', 'var natural_gas_count = ' + total_natural_gas.toString());
+            response = response.replace('var nuclear_count', 'var nuclear_count = ' + total_nuclear_count.toString());
+            response = response.replace('var petroleum_count', 'var petroleum_count = ' + total_petroleum_count.toString());
+            response = response.replace('var renewable_count', 'var renewable_count = ' + total_renewable_count.toString());
+
+            db.all("SELECT * FROM Consumption WHERE year = ?", ["2017"], (err, rows) => {
+                var coalData = '';
+                var gasData = '';
+                var nuclearData = '';
+                var petroleumData = '';
+                var renewableData = '';
+                var tableResult = '';
+                for(var i = 0; i < rows.length; i++)
+                {
+                    coalData = rows[i].coal;
+                    coalData = coalData.toString();
+                    gasData = rows[i].natural_gas;
+                    gasData = gasData.toString();
+                    nuclearData = rows[i].nuclear;
+                    nuclearData = nuclearData.toString();
+                    petroleumData = rows[i].petroleum;
+                    petroleumData = petroleumData.toString();
+                    renewableData = rows[i].renewable;
+                    renewableData = renewableData.toString();
+
+                    tableResult = tableResult + '<tr><td>' + rows[i].state_abbreviation + '</td>' + '\n' + '<td>' + coalData + '</td>' +
+                                '\n' + '<td>' + gasData + '</td>' + '\n' + '<td>' + nuclearData + '</td>' + '\n' + '<td>' +
+                                petroleumData + '</td>' + '\n' + '<td>' + renewableData + '</td></tr>';
+                }
+                response = response.replace('<td>tableData</td>', tableResult);
+                WriteHtml(res, response);
+            });
+            
+        });
+        // db.all for populating table
     }).catch((err) => {
         Write404Error(res);
     });
@@ -40,10 +93,72 @@ app.get('/', (req, res) => {
 
 // GET request handler for '/year/*'
 app.get('/year/:selected_year', (req, res) => {
+    console.log(req.params.selected_year);
+    
     ReadFile(path.join(template_dir, 'year.html')).then((template) => {
         let response = template;
+        // modify response here
+        var total_coal = 0;
+        var total_natural_gas = 0;
+        var total_nuclear_count = 0;
+        var total_petroleum_count = 0;
+        var total_renewable_count = 0;
+        var yearSelected = ''
+        yearSelected = req.params.selected_year;
+        // populate year header
+        response = response.replace('National Snapshot', yearSelected + ' National Snapshot');
+        response = response.replace('var year', 'var year = ' + req.params.selected_year);
+
+        
+        db.all('SELECT * FROM Consumption WHERE year = ?', [yearSelected], (err, rows) => {
+            for (var i = 0; i < rows.length; i++) 
+            {
+                total_coal = total_coal + rows[i].coal;
+                total_natural_gas = total_natural_gas + rows[i].natural_gas;
+                total_nuclear_count = total_nuclear_count + rows[i].nuclear;
+                total_petroleum_count = total_petroleum_count + rows[i].petroleum;
+                total_renewable_count = total_renewable_count + rows[i].renewable;
+            }
+            response = response.replace('var coal_count', 'var coal_count = ' + total_coal.toString());
+            response = response.replace('var natural_gas_count', 'var natural_gas_count = ' + total_natural_gas.toString());
+            response = response.replace('var nuclear_count', 'var nuclear_count = ' + total_nuclear_count.toString());
+            response = response.replace('var petroleum_count', 'var petroleum_count = ' + total_petroleum_count.toString());
+            response = response.replace('var renewable_count', 'var renewable_count = ' + total_renewable_count.toString());
+            db.all("SELECT * FROM Consumption WHERE year = ?", [yearSelected], (err, rows) => {
+                var coalData = '';
+                var gasData = '';
+                var nuclearData = '';
+                var petroleumData = '';
+                var renewableData = '';
+                var totalRow = '';
+
+                var tableResult = '';
+                for(var i = 0; i < rows.length; i++)
+                {
+                    coalData = rows[i].coal;
+                    coalData = coalData.toString();
+                    gasData = rows[i].natural_gas;
+                    gasData = gasData.toString();
+                    nuclearData = rows[i].nuclear;
+                    nuclearData = nuclearData.toString();
+                    petroleumData = rows[i].petroleum;
+                    petroleumData = petroleumData.toString();
+                    renewableData = rows[i].renewable;
+                    renewableData = renewableData.toString();
+                    var totalRowValue = Math.round(coalData + gasData + nuclearData + petroleumData + renewableData);
+                    totalRow = totalRowValue.toString();
+
+                    tableResult = tableResult + '<tr><td>' + rows[i].state_abbreviation + '</td>' + '\n' + '<td>' + coalData + '</td>' +
+                                '\n' + '<td>' + gasData + '</td>' + '\n' + '<td>' + nuclearData + '</td>' + '\n' + '<td>' +
+                                petroleumData + '</td>' + '\n' + '<td>' + renewableData + '\n' + '<td>' + totalRow +'</td>';
+                }
+                response = response.replace('<td>tableData</td>', tableResult);
+                WriteHtml(res, response);
+            });
+        });
+
         // modify `response` here
-        WriteHtml(res, response);
+        
     }).catch((err) => {
         Write404Error(res);
     });
@@ -54,6 +169,20 @@ app.get('/state/:selected_state', (req, res) => {
     ReadFile(path.join(template_dir, 'state.html')).then((template) => {
         let response = template;
         // modify `response` here
+        var total_coal = [];
+        var total_natural_gas = [];
+        var total_nuclear_count = [];
+        var total_petroleum_count = [];
+        var total_renewable_count = [];
+        var state_Chosen = '';
+        state_Chosen = req.params.selected_state;
+        state_Chosen = state_Chosen.toString();
+        console.log(state_Chosen);
+
+        // populate header
+        response = response.replace('<h2>Yearly Snapshot</h2>', '<h2>' + state_Chosen + ' Yearly Snapshot' + '</h2>');
+
+        response = response.replace('var state', 'var state = ' +  "'" + state_Chosen + "'");
         WriteHtml(res, response);
     }).catch((err) => {
         Write404Error(res);
@@ -98,3 +227,4 @@ function WriteHtml(res, html) {
 
 
 var server = app.listen(port);
+
